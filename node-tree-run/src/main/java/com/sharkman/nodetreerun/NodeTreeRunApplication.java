d14e54434f797dd59b862a0b3101f6fab5ed5cab
yanpengyu <yanpengyu@thunisoft.com>
2020-04-17 12:20:41 +0800

    + 添加模块化代码

---
+++ node-tree-run/src/main/java/com/sharkman/nodetreerun/NodeTreeRunApplication.java
@@ -0,0 +1,13 @@
+package com.sharkman.nodetreerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeTreeRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeTreeRunApplication.class, args);
+    }
+
+}
---
+++ node-tree-run/src/test/java/com/sharkman/nodetreerun/NodeTreeRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharkman.nodetreerun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeTreeRunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
