7b39e7891dee169c92d2db4da9854b08c9fe3615
yanpengyu <yanpengyu@thunisoft.com>
2020-04-17 18:13:11 +0800

    + 添加模块化代码

--- node-tree-core/src/test/java/com/sharkman/commons/tree/TreeUtilTest.java
+++ node-tree-core/src/test/java/com/sharkman/commons/tree/TreeUtilTest.java
@@ -1,25 +1,23 @@
 package com.sharkman.commons.tree;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TreeUtilTest {
 
     @Test
     void buildTreeOfRootId() {
         List<Tree> trees = new ArrayList<>();
-
         trees.add(new Tree("1", null));
         trees.add(new Tree("2", "1"));
         trees.add(new Tree("3", "2"));
         trees.add(new Tree("2.1", "1"));
         Tree root = TreeUtil.buildTreeOfRootId(trees, "1");
-        Assertions.assertEquals(root.getId(), "1");
+        assertEquals(root.getId(), "1");
     }
 
     private static class Tree implements Treeable{
