a878b59798c4caad360c4b3cfad7ed30db56d5c6
燕鹏宇 <yanpengyu@thunisoft.com>
2024-04-19 16:23:31 +0800

    :bulb: 修改注解版本

--- node-tree-annotation/src/main/java/com/sharkman/nodetree/annotation/Rights.java
+++ node-tree-annotation/src/main/java/com/sharkman/nodetree/annotation/Rights.java
@@ -6,7 +6,7 @@ package com.sharkman.nodetree.annotation;
  * <p> CreationTime: 2023/1/4 11:20
  * @author piwood
  * @version 1.0
- * @since 3.0.0
+ * @since 2.0.0
  */
 public @interface Rights {
 }
